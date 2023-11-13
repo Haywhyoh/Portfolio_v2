52a74e2670bf3632064a8abe7d80363d11fce3d6
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-11-13 21:27:54 +0100

    fist phase of the html design

--- assets/js/minibar-virtual.js
+++ assets/js/minibar-virtual.js
@@ -214,4 +214,18 @@ $(window).on("load", function() {
           }
       }), !1
   });
+
+  if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+		var typed = new Typed('.text-slider', {
+			strings: typed_strings.split(','),
+			typeSpeed: 80,
+			loop: true,
+			backDelay: 1100,
+			backSpeed: 30
+		});
+	}
 });
+
+	/*--/ Star Typed /--*/
+
